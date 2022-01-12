b98ca7162f7367047c628fe3f974302b16e437f5
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-01-12 20:19:17 -0300

    criando DetailViewController

---
+++ UIKit/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Jean Camargo on 12/01/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
