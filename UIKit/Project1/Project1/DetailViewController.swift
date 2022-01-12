d37c72c576ad7bdae5b539014d4b9b94b1e53682
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-01-12 20:49:42 -0300

    conectando UIImageView ao código de DetailViewController

--- UIKit/Project1/Project1/DetailViewController.swift
+++ UIKit/Project1/Project1/DetailViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet var imageView: UIImageView!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
