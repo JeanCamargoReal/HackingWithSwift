618b860b21a6ad65289380185a59e3bf54811b17
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-02-07 10:17:41 -0300

    configurando Project5

---
+++ UIKit/Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Jean Camargo on 07/02/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
