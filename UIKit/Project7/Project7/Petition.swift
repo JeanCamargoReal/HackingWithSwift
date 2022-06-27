7ccfd25e18d44a5ee8aae6667318058431c81fd4
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-06-27 14:51:39 -0300

    Coloca dados de uma API na UITableView

---
+++ UIKit/Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 27/06/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ UIKit/Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 27/06/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
